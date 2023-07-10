6e1e642b2195d52f18473db38b2310780aeccef4
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-07-09 17:09:04 -0700

    getToDosIncomplete and addToDo - draft APIs working

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,19 +1,50 @@
+import { toDos } from "../models/toDosSchema.js";
+
+const sampleList = [
+  {
+    id: 1,
+    content: "Uno",
+    done: false,
+  },
+  {
+    id: 2,
+    content: "Dos",
+    done: true,
+  },
+];
+
 /* Get all To Do's */
+export async function getToDosIncomplete(req, res) {
+  // res.json(
+  //   `To Dos API get Incomplete all - GET request: searchTerm is ${req.query.searchTerm}`
+  // );
+
+  try {
+    // find all elements inside To Do's collection
+    const list = await toDos.find();
+    res.json(list);
+  } catch (error) {
+    res.json({ error });
+  }
+}
+
 export async function getToDosDone(req, res) {
   res.json(
     `To Dos API get Done latest 10 items - GET request: searchTerm is ${req.query.searchTerm}`
   );
 }
 
-export async function getToDosIncomplete(req, res) {
-  res.json(
-    `To Dos API get Incomplete all - GET request: searchTerm is ${req.query.searchTerm}`
-  );
-}
-
 /* Add/Insert a To Do */
 export async function addToDo(req, res) {
-  res.json("To Dos API add - POST request");
+  // res.json("To Dos API add - POST request");
+
+  try {
+    toDos
+      .insertMany({ list: sampleList })
+      .then(res.json({ msg: "Data saved successfully!" }));
+  } catch (error) {
+    res.json({ error });
+  }
 }
 
 /* Update a To Do */
---
+++ models/toDosSchema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+/* To Do's collection model */
+const toDosModel = new Schema({
+  list: { type: Array, default: [] },
+});
+
+// const toDoModel = new Schema({
+//   id: { type: Number, default: 0 },
+//   createdAt: { type: Date, default: Date.now },
+//   content: { type: String, default: "" },
+//   done: { type: Boolean, default: false },
+// });
+
+export const toDos = mongoose.model("ToDos", toDosModel);
