228ba251dcd733c3eaa232a832f0affc4a109a6d
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-10-02 16:17:18 -0700

    conn.js: prettier fix

--- src/database/conn.js
+++ src/database/conn.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connect() {
   await mongoose.connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`,
   );
   console.log("Database connected!");
 }
-
